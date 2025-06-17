4140ec2d501a0df521692161b77781996b4bc9aa
PedroHenrique2204 <pedrorft22@gmail.com>
2025-06-16 23:53:34 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Página não encontrada');
+  }
+});
+
+// Porta onde o servidor vai escutar
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
